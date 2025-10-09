1860be12324e0995fbb131d969d5874c5b4af132
kayla <yuwon.lee@tufts.edu>
2025-10-09 15:56:14 -0400

    10/9 meeting

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
 
 app = FastAPI()
 
@@ -12,16 +13,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# store latest water data in memory
+latest_water_data = {"timestamp": None, "water_level": None}
+
+# this endpoint runs when you do a GET request to /water
+# frontend calls this to get data
 @app.get("/water")
 def get_water_level():
-    # temporary placeholder data
-    return {
-        "timestamp": "2025-10-06T00:00:00Z",
-        "water_level": 121.3
-    }
+    if latest_water_data["timestamp"] is None:
+        return {"message": "No data received yet."}
+    return latest_water_data
 
+
+# runs a POST request to /water
+# reads JSON sent in the body of the request, prints it, and replies with a confirmation
 @app.post("/water")
 async def receive_data(request: Request):
     data = await request.json()  # read JSON body
     print("📥 Received data:", data)
+
+     # update the global latest_water_data variable
+    latest_water_data["timestamp"] = datetime.utcnow().isoformat() + "Z"
+    latest_water_data["water_level"] = data.get("water_level")
+
     return {"status": "ok", "message": "Data received successfully"}
--- frontend/Readme.txt
+++ frontend/Readme.txt
@@ -1,14 +1,15 @@
 
 Use data fetched:
 
-    - Display the water level to be used by the organization
+    - Display the water level to be used by the organization or community in Malawi
     - 
-    - 
-    - 
-    - 
+Water level: is for the tank and determine, tank is full or not, how level keep changing
 
 customers (open to everyone):
     - organizations
     - Community.
-
+Note: 
+    - For the app, we will keep displaying the most recent but also keep the future data
+    - Malawi Colors: Red, green, and black.
+    - Need the copyright from EWB developers 
 Design with Figma.
